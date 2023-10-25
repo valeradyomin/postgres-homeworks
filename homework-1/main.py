31626e1dde3677cfa72316860010fe4737d3204c
Valera <wahrwolffe@gmail.com>
2023-10-25 04:06:40 +0300

    test db

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,17 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+# release connection to database
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="1739")
+try:
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", ('ALFKI', 'Alfreds Futterkiste', 'Maria Anders'))
+            cur.execute("SELECT * FROM customers")
+
+            # print results
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+finally:
+    conn.close()
